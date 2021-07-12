e4f0296e795b8646bcb6cf89676f86468b819d61
vj <vijaymsvpes@gmail.com>
2021-07-12 22:24:49 +0530

    ninitial commit

---
+++ dsa/tuple.py
@@ -0,0 +1,14 @@
+mytuple = ("aa", 38, "vijay")
+print(mytuple)
+
+#optional para
+mytuple1 = "bb", 38, "vijay"
+print(mytuple1)
+
+#single element
+mytuple2 = ("aa",)
+print(mytuple2)
+
+#without , - its string
+myStr = ("aa")
+print(type(myStr))
